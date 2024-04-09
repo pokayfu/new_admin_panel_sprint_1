0d3a35f2d3523095cc21172990f5d15fd855e8b7
g.biryukov <g.biryukov@rt-ib.ru>
2024-04-09 11:52:41 +0300

    сократил как мог

--- sqlite_to_postgres/main.py
+++ sqlite_to_postgres/main.py
@@ -3,13 +3,20 @@ import sqlite3
 import psycopg2
 from insert_data import InsertWorker
 from settings import dsn, sql_path
-from contextlib import contextmanager
+from contextlib import contextmanager, closing
 from tests.check_consistency.tests import TestWorker
 
 
 class SQLToPostgres:
     def __init__(self) -> None:
-        pass
+        self.Writer = InsertWorker()
+        self.table_name_insert_function = {
+            'person': self.Writer.insert_person,
+            'film_work': self.Writer.insert_film_work,
+            'genre': self.Writer.insert_genre,
+            'genre_film_work': self.Writer.insert_genre_film_work_data,
+            'person_film_work': self.Writer.insert_person_film_work_data
+        }
 
     @contextmanager
     def conn_context(self, db_path: str):
@@ -21,33 +28,12 @@ class SQLToPostgres:
             conn.close()
 
     def run(self) -> None:
-        with self.conn_context(db_path=sql_path) as conn, psycopg2.connect(**dsn) as pgconn:
+        with self.conn_context(db_path=sql_path) as conn, closing(psycopg2.connect(**dsn)) as pgconn:
             sqlite_curs = conn.cursor()
             pgcursor = pgconn.cursor()
-            Writer = InsertWorker()
-
-            for person_row in load_from_sqlite(sqlite_curs,
-                                               table_name='person'):
-                Writer.insert_person(cursor=pgcursor, item=person_row)
-
-            for film_work_row in load_from_sqlite(sqlite_curs,
-                                                  table_name='film_work'):
-                Writer.insert_film_work(cursor=pgcursor,
-                                        item=film_work_row)
-
-            for genre_row in load_from_sqlite(sqlite_curs, table_name='genre'):
-                Writer.insert_genre(cursor=pgcursor, item=genre_row)
-
-            for gwf_row in load_from_sqlite(sqlite_curs,
-                                            table_name='genre_film_work'):
-                Writer.insert_genre_film_work_data(cursor=pgcursor,
-                                                   item=gwf_row)
-
-            for pfw_data in load_from_sqlite(sqlite_curs,
-                                             table_name='person_film_work'):
-                Writer.insert_person_film_work_data(cursor=pgcursor,
-                                                    item=pfw_data)
-        pgconn.close()
+            for table, insert_foo in self.table_name_insert_function.items():
+                for table_row in load_from_sqlite(sqlite_curs, table_name=table):
+                    insert_foo(cursor=pgcursor, item=table_row)
         conn.close()
 
 
